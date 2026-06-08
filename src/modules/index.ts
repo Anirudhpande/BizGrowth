00e1b2dbeaf446b787f0dc295318004ef8bace5a
Varadc05 <varadchidrewar5@gmail.com>
2026-06-08 21:29:10 +0530

    Delete src/modules directory

--- src/modules/index.ts
+++
@@ -1,5 +0,0 @@
-import bookingsRouter from './bookings/bookings.route';
-import reviewsRouter from './reviews/reviews.route';
-import categoriesRouter from './categories/categories.route';
-
-export { bookingsRouter, reviewsRouter, categoriesRouter };
