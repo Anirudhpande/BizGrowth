0c3f214719bfc81c79b3f0c944f62bc197a90f70
Varadc05 <varadchidrewar5@gmail.com>
2026-06-08 12:37:56 +0530

    feat: implement Phase 2 core backend modules (Bookings, Reviews, Categories)

---
+++ src/modules/index.ts
@@ -0,0 +1,5 @@
+import bookingsRouter from './bookings/bookings.route';
+import reviewsRouter from './reviews/reviews.route';
+import categoriesRouter from './categories/categories.route';
+
+export { bookingsRouter, reviewsRouter, categoriesRouter };
